28033c361e42b39b4a500c70f7892c60b9bd4cf1
Tyler Hagerty <thagerty_0213@email.ric.edu>
2015-11-09 15:05:13 -0500

    Tableau Data Structure
    Coded most of the operations/functionality I want for the tableau data
    type so far such as pivoting, redoing/undoing, etc. May change the
    structure soon to having Tableau as an interface and having
    TuckerTableau and SimplexTableau as data types that implement Tableau
    (then they'd need constructors that accept the opposite type to allow
    the conversion instead).
    
    Also did a tiny bit on starting to setup a GUI, but not much, and not
    final at all.

---
+++ Tableau/tableau/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tableau
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// Initializes the main window that will hold all the views of the program within a stackpanel
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainMenu mm = new MainMenu();
+            WindowPanel.Children.Add(mm);
+        }
+    }
+}
